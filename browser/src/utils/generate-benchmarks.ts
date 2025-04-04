bb4ec00df8a34bc1dc1e8c2d08864bab1fd5006e
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2025-04-04 17:54:08 -0600

    Browser benchmarks for Noir functionality

--- browser/src/utils/generate-benchmarks.ts
+++ browser/src/utils/generate-benchmarks.ts
@@ -1,8 +1,6 @@
 import { Bench, Task } from "tinybench"
-import { Identity } from "@semaphore-protocol/identity"
-import { Group } from "@semaphore-protocol/group"
-import { generateProof } from "@semaphore-protocol/proof"
 import * as V4 from "@semaphore-protocol/core"
+import { generateNoirProof } from "@semaphore-noir-protocol/proof"
 
 const generateTable = (task: Task) => {
   if (task && task.name && task.result) {
@@ -20,18 +18,7 @@ const generateTable = (task: Task) => {
 }
 
 export async function generateBenchmarks() {
-  //   const samples = 100
-
-  //   const bench = new Bench({ time: 0, iterations: samples })
-
   const bench = new Bench()
-
-  const generateMembersV3 = (size: number) => {
-    return Array.from(
-      { length: size },
-      (_, i) => new Identity(i.toString())
-    ).map(({ commitment }) => commitment)
-  }
 
   const generateMembersV4 = (size: number) => {
     return Array.from(
@@ -40,65 +27,19 @@ export async function generateBenchmarks() {
     ).map(({ commitment }) => commitment)
   }
 
-  let memberV3: Identity
-
   let memberV4: V4.Identity
-
-  let groupV3: Group
 
   let groupV4: V4.Group
 
-  let membersV3: bigint[]
-
   let membersV4: bigint[]
-
-  const secretMessage = "secret-message"
 
   let v4Identity: V4.Identity
 
-  const message = "Message to sign"
-
   bench
-    .add("V3 - Create non-deterministic Identity", async () => {
-      new Identity()
-    })
-    .add("V4 - Create non-deterministic Identity", async () => {
-      new V4.Identity()
-    })
-    .add("V3 - Create deterministic Identity", async () => {
-      new Identity(secretMessage)
-    })
-    .add("V4 - Create deterministic Identity", async () => {
-      new V4.Identity(secretMessage)
-    })
     .add(
-      "V4 - Sign Message",
+      "Noir - Generate Proof 1 Member",
       async () => {
-        v4Identity.signMessage(message)
-      },
-      {
-        beforeAll: () => {
-          v4Identity = new V4.Identity()
-        }
-      }
-    )
-    .add(
-      "V3 - Generate Proof 1 Member",
-      async () => {
-        await generateProof(memberV3, groupV3, 1, 1)
-      },
-      {
-        beforeAll: () => {
-          groupV3 = new Group(1, 16, [])
-          memberV3 = new Identity()
-          groupV3.addMember(memberV3.commitment)
-        }
-      }
-    )
-    .add(
-      "V4 - Generate Proof 1 Member",
-      async () => {
-        await V4.generateProof(memberV4, groupV4, 1, 1)
+        await generateNoirProof(memberV4, groupV4, 1, 1, undefined, undefined, { threads: navigator.hardwareConcurrency ?? 1 })
       },
       {
         beforeAll: () => {
@@ -109,23 +50,9 @@ export async function generateBenchmarks() {
       }
     )
     .add(
-      "V3 - Generate Proof 100 Members",
+      "Noir - Generate Proof 100 Members",
       async () => {
-        await generateProof(memberV3, groupV3, 1, 1)
-      },
-      {
-        beforeAll: () => {
-          membersV3 = generateMembersV3(100)
-          groupV3 = new Group(1, 16, membersV3)
-          const index = Math.floor(membersV3.length / 2)
-          memberV3 = new Identity(index.toString())
-        }
-      }
-    )
-    .add(
-      "V4 - Generate Proof 100 Members",
-      async () => {
-        await V4.generateProof(memberV4, groupV4, 1, 1)
+        await generateNoirProof(memberV4, groupV4, 1, 1,undefined, undefined,  { threads: navigator.hardwareConcurrency ?? 1 })
       },
       {
         beforeAll: () => {
@@ -137,23 +64,9 @@ export async function generateBenchmarks() {
       }
     )
     .add(
-      "V3 - Generate Proof 500 Members",
+      "Noir - Generate Proof 500 Members",
       async () => {
-        await generateProof(memberV3, groupV3, 1, 1)
-      },
-      {
-        beforeAll: () => {
-          membersV3 = generateMembersV3(500)
-          groupV3 = new Group(1, 16, membersV3)
-          const index = Math.floor(membersV3.length / 2)
-          memberV3 = new Identity(index.toString())
-        }
-      }
-    )
-    .add(
-      "V4 - Generate Proof 500 Members",
-      async () => {
-        await V4.generateProof(memberV4, groupV4, 1, 1)
+        await generateNoirProof(memberV4, groupV4, 1, 1, undefined, undefined, { threads: navigator.hardwareConcurrency ?? 1 })
       },
       {
         beforeAll: () => {
@@ -165,23 +78,9 @@ export async function generateBenchmarks() {
       }
     )
     .add(
-      "V3 - Generate Proof 1000 Members",
+      "Noir - Generate Proof 1000 Members",
       async () => {
-        await generateProof(memberV3, groupV3, 1, 1)
-      },
-      {
-        beforeAll: () => {
-          membersV3 = generateMembersV3(1000)
-          groupV3 = new Group(1, 16, membersV3)
-          const index = Math.floor(membersV3.length / 2)
-          memberV3 = new Identity(index.toString())
-        }
-      }
-    )
-    .add(
-      "V4 - Generate Proof 1000 Members",
-      async () => {
-        await V4.generateProof(memberV4, groupV4, 1, 1)
+        await generateNoirProof(memberV4, groupV4, 1, 1, undefined, undefined, { threads: navigator.hardwareConcurrency ?? 1 })
       },
       {
         beforeAll: () => {
@@ -193,23 +92,9 @@ export async function generateBenchmarks() {
       }
     )
     .add(
-      "V3 - Generate Proof 2000 Members",
+      "Noir - Generate Proof 2000 Members",
       async () => {
-        await generateProof(memberV3, groupV3, 1, 1)
-      },
-      {
-        beforeAll: () => {
-          membersV3 = generateMembersV3(2000)
-          groupV3 = new Group(1, 16, membersV3)
-          const index = Math.floor(membersV3.length / 2)
-          memberV3 = new Identity(index.toString())
-        }
-      }
-    )
-    .add(
-      "V4 - Generate Proof 2000 Members",
-      async () => {
-        await V4.generateProof(memberV4, groupV4, 1, 1)
+        await generateNoirProof(memberV4, groupV4, 1, 1, undefined, undefined, { threads: navigator.hardwareConcurrency ?? 1 })
       },
       {
         beforeAll: () => {
@@ -225,37 +110,6 @@ export async function generateBenchmarks() {
   await bench.run()
 
   const table = bench.table((task) => generateTable(task))
-
-  // Add column to show how many times V4 is faster/slower than V3.
-  // Formula: highest average execution time divided by lowest average execution time.
-  // Using highest ops/sec divided by lowest ops/sec would work too.
-  table.map((rowInfo, i) => {
-    if (rowInfo && !(rowInfo["Function"] as string).includes("V4")) {
-      rowInfo["Relative to V3"] = ""
-    } else if (rowInfo && (rowInfo["Function"] as string).includes("Sign")) {
-      rowInfo["Relative to V3"] = "N/A"
-    } else if (rowInfo) {
-      const v3AvgExecTime = bench.tasks[i - 1].result?.mean
-
-      const v4AvgExecTime = bench.tasks[i]!.result?.mean
-
-      if (v3AvgExecTime === undefined || v4AvgExecTime === undefined) return
-
-      if (v3AvgExecTime > v4AvgExecTime) {
-        rowInfo["Relative to V3"] = `${(v3AvgExecTime / v4AvgExecTime).toFixed(
-          2
-        )} x faster`
-      } else {
-        rowInfo["Relative to V3"] = `${(v4AvgExecTime / v3AvgExecTime).toFixed(
-          2
-        )} x slower`
-      }
-    }
-  })
-
-  // console.table(table)
-
-  // console.log(bench.results)
 
   return table
 }
